523fe6b557d1676345f9cb0166f1691faca62451
Danil Shaymurzin <d.shaymurzin@aliase.ru>
2019-12-31 14:55:09 +0500

    Fix

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-const name: string = 'Danil';
-
-global.console.log(`Hello, my name is ${name}. Nice to meet you.`);
--- src/test/sum.spec.ts
+++
@@ -1,9 +0,0 @@
-import sum from '../sum'
-
-test('empty adds equal to 0', () => {
-    expect(sum()).toBe(0)
-})
-
-test('...adds equal to sum', () => {
-    expect(sum(1, 2)).toBe(3)
-})
